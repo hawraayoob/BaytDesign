b4124fb11c2f8d3506f9a9e2aee3e99723089bf0
Hawra ayoob <hawraalkulaib.01@gmail.com>
2025-09-17 15:16:14 +0300

    ip

--- Baytdesign/settings.py
+++ Baytdesign/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "main_app" / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "main_app" / "static",
+]
 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
